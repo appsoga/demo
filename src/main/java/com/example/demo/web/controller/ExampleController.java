5351b1741a06cd2e7f4293ce82bc8c0aad7f1f9b
sangmok <appsoga@gmail.com>
2019-11-04 08:13:09 +0900

    예제 경로 분리

--- src/main/java/com/example/demo/web/controller/ExampleController.java
+++ src/main/java/com/example/demo/web/controller/ExampleController.java
@@ -3,16 +3,19 @@ package com.example.demo.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping(value = "example")
 @Controller
+@RequestMapping(value = "example")
 public class ExampleController {
 
     /**
      * datatable의 예제
      */
     @RequestMapping(value = "datatable")
-    public String app_datatable_html() {
-        return "datatable";
+    public void app_datatable_html() {
+    }
+
+    @RequestMapping(value = "chartjs")
+    public void app_chartjs_html() {
     }
 
 }
