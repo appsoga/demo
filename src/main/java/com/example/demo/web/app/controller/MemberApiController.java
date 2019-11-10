49f325581c3dc61c81a8ac52f23618c3c06067a2
sangmok <appsoga@gmail.com>
2019-11-10 09:19:02 +0900

    암튼 삽질은 싫다.

--- src/main/java/com/example/demo/web/app/controller/MemberApiController.java
+++ src/main/java/com/example/demo/web/app/controller/MemberApiController.java
@@ -8,9 +8,6 @@
  */
 package com.example.demo.web.app.controller;
 
-import com.example.demo.data.Member;
-import com.example.demo.service.MemberService;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,66 +23,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import sangmok.util.jsgrid.JsGridRequest;
-import sangmok.util.jsgrid.JsGridResponse;
+import com.example.demo.data.Member;
+import com.example.demo.service.MemberService;
 
 @RestController
 @RequestMapping(path = "api/members")
 public class MemberApiController {
 
-    private static Logger logger = LoggerFactory.getLogger(MemberApiController.class);
+	private static Logger logger = LoggerFactory.getLogger(MemberApiController.class);
 
-    @Autowired
-    private MemberService memberService;
+	@Autowired
+	private MemberService memberService;
 
-    @GetMapping(path = "jsgrid")
-    public ResponseEntity<?> jsGrid(@RequestBody JsGridRequest jsr, @RequestBody Member filter,
-            UriComponentsBuilder ucBuilder) {
-        logger.info("jtable request is {}, filter is {}", jsr, filter);
-        // logger.info("jtable request is {}, filter is {}", jsr);
-        JsGridResponse<Member> jtr = memberService.getMembersForJsGrid(jsr, filter);
-        return new ResponseEntity<JsGridResponse<Member>>(jtr, HttpStatus.OK);
-    }
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody Member e1, UriComponentsBuilder ucBuilder) {
+		logger.info("create member: {}", e1);
+		if (e1.getUsername() == null || e1.getUsername().isEmpty()) {
+			return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+		}
+		e1 = memberService.createMember(e1);
+		return new ResponseEntity<Member>(e1, HttpStatus.OK);
+	}
 
-    @PostMapping
-    public ResponseEntity<?> create(@RequestBody Member e1, UriComponentsBuilder ucBuilder) {
-        logger.info("create member: {}", e1);
-        if (e1.getUsername() == null || e1.getUsername().isEmpty()) {
-            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
-        }
-        e1 = memberService.createMember(e1);
-        return new ResponseEntity<Member>(e1, HttpStatus.OK);
-    }
+	@GetMapping(path = "{id}")
+	public ResponseEntity<?> read(@PathVariable(name = "id") Integer id) {
+		if (id == null)
+			return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+		logger.debug("request id: {}", id);
+		Member e1 = memberService.getMember(id);
+		if (e1 == null)
+			return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
+		return new ResponseEntity<Member>(e1, HttpStatus.OK);
+	}
 
-    @GetMapping(path = "{id}")
-    public ResponseEntity<?> read(@PathVariable(name = "id") Integer id) {
-        if (id == null)
-            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
-        logger.debug("request id: {}", id);
-        Member e1 = memberService.getMember(id);
-        if (e1 == null)
-            return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
-        return new ResponseEntity<Member>(e1, HttpStatus.OK);
-    }
+	@PutMapping
+	public ResponseEntity<?> update(@RequestBody Member e1, UriComponentsBuilder ucBuilder) {
+		logger.info("update member: {}", e1);
+		if (e1.getId() == null || e1.getUsername() == null)
+			return new ResponseEntity<Member>(e1, HttpStatus.BAD_REQUEST);
+		e1 = memberService.modifyMember(e1);
+		if (e1 == null)
+			return new ResponseEntity<Member>(e1, HttpStatus.NOT_FOUND);
+		return new ResponseEntity<Member>(e1, HttpStatus.OK);
+	}
 
-    @PutMapping
-    public ResponseEntity<?> update(@RequestBody Member e1, UriComponentsBuilder ucBuilder) {
-        logger.info("update member: {}", e1);
-        if (e1.getId() == null || e1.getUsername() == null)
-            return new ResponseEntity<Member>(e1, HttpStatus.BAD_REQUEST);
-        e1 = memberService.modifyMember(e1);
-        if (e1 == null)
-            return new ResponseEntity<Member>(e1, HttpStatus.NOT_FOUND);
-        return new ResponseEntity<Member>(e1, HttpStatus.OK);
-    }
-
-    @DeleteMapping(path = "{id}")
-    public ResponseEntity<?> delete(@PathVariable(name = "id") Integer id) {
-        if (id == null)
-            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
-        memberService.removeMember(id);
-        logger.debug("deleted id: {}", id);
-        return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
-    }
+	@DeleteMapping(path = "{id}")
+	public ResponseEntity<?> delete(@PathVariable(name = "id") Integer id) {
+		if (id == null)
+			return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+		memberService.removeMember(id);
+		logger.debug("deleted id: {}", id);
+		return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+	}
 
 }
