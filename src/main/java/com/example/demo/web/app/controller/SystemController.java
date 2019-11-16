4d2675693d498d70d65366fde8015513447a5d75
sangmok <appsoga@gmail.com>
2019-11-16 23:40:04 +0900

    회원관리 변경

---
+++ src/main/java/com/example/demo/web/app/controller/SystemController.java
@@ -0,0 +1,26 @@
+package com.example.demo.web.app.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = { "app/system" })
+@Controller
+public class SystemController {
+
+	private static Logger logger = LoggerFactory.getLogger(MemberController.class);
+
+	@RequestMapping(value = { "" })
+	public String _system(Model model) {
+		return "redirect:./members.html";
+	}
+
+	@RequestMapping(value = "members.html")
+	public void members_html(Model model) {
+		logger.debug("model: {}", model);
+
+	}
+
+}
